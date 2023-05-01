a9d0bb71e6f89914377ac90d919da5d8246bd73c
Shlok <saraogishlok5@gmail.com>
2023-05-01 22:50:41 +0530

    Added Frontend UI for home page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '@/Components/Navbar/Navbar';
 import AuthModal from '@/Components/Modals/AuthModal';
 import { AuthModalState } from '@/atoms/authModalAtom';
 import { useRecoilValue } from 'recoil';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
 
 type AuthPageProps = {
-    
+
 };
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     
     const authModal = useRecoilValue(AuthModalState);
     const [user,loading,error] = useAuthState(auth);
+    const [pageLoading,setPageLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        if(user) router.push('/');
+        if(!loading && !user) setPageLoading(false);
+    },[user,router,loading]);
+
+    if(pageLoading) return null;
 
     return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
         <div className="max-w-7xl mx-auto">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,46 @@
-import { Inter } from 'next/font/google'
+import Topbar from '@/Components/Topbar/Topbar';
+import { Inter } from 'next/font/google';
+// import {ProblemsTable} from '@/Components/ProblemsTable/ProblemsTable';
+import ProblemsTable from '@/Components/ProblemsTable/ProblemsTable';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <main>
-        <h1>Hello World</h1>
+      <main className='bg-dark-layer-2 min-h-screen'>
+        <Topbar/>
+        <h1
+					className='text-2xl text-center text-gray-700 dark:text-gray-400 font-medium
+					uppercase mt-10 mb-5'
+				>
+					&ldquo; QUALITY OVER QUANTITY &rdquo; 👇
+				</h1>
+				<div className='relative overflow-x-auto mx-auto px-6 pb-10'>
+					<table className='text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto'>
+						<thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
+							<tr>
+								<th scope='col' className='px-1 py-3 w-0 font-medium'>
+									Status
+								</th>
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Title
+								</th>
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Difficulty
+								</th>
+
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Category
+								</th>
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Solution
+								</th>
+							</tr>
+						</thead>
+            <ProblemsTable/>
+					</table>
+				</div>
       </main>
     </>
   )
